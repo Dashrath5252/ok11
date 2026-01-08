26789341bf62994a43846bac4026ce6ec24be7aa
Dashrath5252 <paraliyadashrath5252@gmail.com>
2026-01-08 21:33:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: false, // ✅ Resize ON
+    unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
   },
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Build errors रोकेगा
+    ignoreDuringBuilds: true,
   },
 }
 
