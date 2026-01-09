41d349da652b4700aeb6ae78824596a8437a10bd
Dashrath5252 <paraliyadashrath5252@gmail.com>
2026-01-09 21:36:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 export const metadata = {
-  title: "OK11 Game APK Download",
-  description: "Official OK11 Game website",
+  title: "OK11 Game APK Download - Official website | ₹500 Bonus",
+  description:
+    "ok11 Game offers exciting slot games for entertainment. Download now.",
+  robots: "index, follow",
+  openGraph: {
+    title: "ok11 Game APK",
+    description: "Free download ok11 Game APK",
+    images: ["/ok11icon.webp"],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
